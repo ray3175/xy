ee7d2108c0ce2f99db357ce27c89f1c39a7d35e8
ray3175 <57464715@qq.com>
2019-08-02 02:20:29 +0800

    新增qrcode

---
+++ xy/utils/_qrcode.py
@@ -0,0 +1,16 @@
+import qrcode
+
+
+class QRCode:
+    def __init__(self, version=None, box_size=2, border=1):
+        """
+        :param params: 例，{"box_size": 2, "border": 1}
+        """
+        self.__qrcode = qrcode.QRCode(version, box_size=box_size, border=border)
+
+    def new(self, data, save_path, optimize=0):
+        self.__qrcode.clear()
+        self.__qrcode.add_data(data, optimize)
+        self.__qrcode.make()
+        self.__qrcode.make_image().save(save_path)
+
