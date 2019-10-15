9c988b13c58becc57a9ddcfdeccd99d31c052fca
伪善者 <39792724+ray3175@users.noreply.github.com>
2019-10-15 09:56:15 +0800

    Update array.py
    修改ArrayErgodic遍历算法。。。

--- xy/arith/binary_tree/ergodic/array.py
+++ xy/arith/binary_tree/ergodic/array.py
@@ -4,29 +4,16 @@ from . import Ergodic
 class ArrayErgodic(Ergodic):
     @classmethod
     def depth_first_ergodic(cls, array):
-        temp_array = list()
-        if array:
-            for i in array[0]:
-                if array[1:]:
-                    for j in cls.depth_first_ergodic(array[1:]):
-                        temp_array.append([i, *j])
-                else:
-                    temp_array.append([i])
-        return temp_array
+        if len(array) > 1:
+            return [[i, *j] for i in array[0] for j in cls.new_array_depth_first_ergodic(array[1:])]
+        else:
+            return array
 
     @classmethod
     def breadth_first_ergodic(cls, array):
-        temp_array = list()
-        if array:
-            temp_list = list()
-            for i in array[0]:
-                temp_list.append(i)
-            if array[1:]:
-                for j in cls.breadth_first_ergodic(array[1:]):
-                    for k in temp_list:
-                        temp_array.append([k, *j])
-            else:
-                temp_array.extend([[i] for i in temp_list])
-        return temp_array
+        if len(array) > 1:
+            return [[i, *j] for j in cls.new_array_breadth_first_ergodic(array[1:]) for i in array[0]]
+        else:
+            return array
 
 
