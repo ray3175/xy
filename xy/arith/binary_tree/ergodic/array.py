acd6b36a9728aac5cea84afaba91744a845e82ca
伪善者 <39792724+ray3175@users.noreply.github.com>
2019-10-15 11:23:31 +0800

    Update array.py

--- xy/arith/binary_tree/ergodic/array.py
+++ xy/arith/binary_tree/ergodic/array.py
@@ -5,14 +5,14 @@ class ArrayErgodic(Ergodic):
     @classmethod
     def depth_first_ergodic(cls, array):
         if len(array) > 1:
-            return [[i, *j] for i in array[0] for j in cls.new_array_depth_first_ergodic(array[1:])]
+            return [[i, *j] for i in array[0] for j in cls.depth_first_ergodic(array[1:])]
         else:
             return array
 
     @classmethod
     def breadth_first_ergodic(cls, array):
         if len(array) > 1:
-            return [[i, *j] for j in cls.new_array_breadth_first_ergodic(array[1:]) for i in array[0]]
+            return [[i, *j] for j in cls.breadth_first_ergodic(array[1:]) for i in array[0]]
         else:
             return array
 
