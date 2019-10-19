e2269937318d39043b890c3c60deb92dbd3102e9
ray3175 <57464715@qq.com>
2019-10-20 01:00:38 +0800

    新增pd2excel

--- xy/arith/pandas/core.py
+++ xy/arith/pandas/core.py
@@ -22,6 +22,28 @@ class Core:
         return pandas.DataFrame(_dict)
 
     @classmethod
+    def get_pd_columns(cls, pd):
+        """
+        :param pd: pandas.core.frame.DataFrame
+        :return: []
+        """
+        _return = list()
+        if IsPd.is_pd(pd):
+            _return = np_core.np2list(pd.columns.values)
+        return _return
+
+    @classmethod
+    def get_pd_values(cls, pd):
+        """
+        :param pd: pandas.core.frame.DataFrame
+        :return: [[], [], ...]
+        """
+        _return = None
+        if IsPd.is_pd(pd):
+            _return = np_core.np2list(pd.values)
+        return _return
+
+    @classmethod
     def pd2dict(cls, pd):
         """
         :param pd: pandas.core.frame.DataFrame
@@ -48,23 +70,26 @@ class Core:
         return _return
 
     @classmethod
-    def get_pd_columns(cls, pd):
+    def pd2excel(cls, pd, path, sheet_name="Sheet1", default="", float_format=None, index=True, index_label=None, encoding="utf-8"):
         """
+        # pip install openpyxl
         :param pd: pandas.core.frame.DataFrame
-        :return: []
-        """
-        _return = list()
-        if IsPd.is_pd(pd):
-            _return = np_core.np2list(pd.columns.values)
-        return _return
-
-    @classmethod
-    def get_pd_values(cls, pd):
-        """
-        :param pd: pandas.core.frame.DataFrame
-        :return: [[], [], ...]
+        :param path: excel路径。
+        :param sheet_name: Sheet名称。
+        :param default: 缺少数据默认值。
+        :param float_format: 浮点数格式字符串，例：'%.2f'。
+        :param index: 是否有索引列。
+        :param index_label: 索引列的标签。
+        :param encoding: 文件编码格式。
+        :return: True or False
         """
         _return = None
         if IsPd.is_pd(pd):
-            _return = np_core.np2list(pd.values)
+            try:
+                pd.to_excel(path, sheet_name, default, float_format, index=index, index_label=index_label, encoding=encoding)
+                _return = True
+            except:
+                _return = False
         return _return
+
+
