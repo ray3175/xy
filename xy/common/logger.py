fe5bbbf800e3492c1e1d4c8bbb2ddef18039c595
ray3175 <57464715@qq.com>
2019-11-06 22:15:16 +0800

    新增基于时间的日志功能

--- xy/common/logger.py
+++ xy/common/logger.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import logging
+import logging.handlers
 
 
 class Logger:
@@ -18,13 +19,15 @@ class Logger:
                  name=os.path.split(os.path.splitext(sys.argv[0])[0])[-1],
                  log_name=time.strftime("%Y-%m-%d.log", time.localtime()),
                  log_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "log"),
-                 use_console=True):
+                 use_console=True,
+                 use_timed_rotating=True, timed_rotating_params={}):
         """
         :param set_level: 日志级别["NOTSET"|"DEBUG"|"INFO"|"WARNING"|"ERROR"|"CRITICAL"]，默认为INFO
         :param name: 日志中打印的name，默认为运行程序的name
         :param log_name: 日志文件的名字，默认为当前时间（年-月-日.log）
         :param log_path: 日志文件夹的路径，默认为logger.py同级目录中的log文件夹
         :param use_console: 是否在控制台打印，默认为True
+        :param use_timed_rotating: 是否基于时间输出日志，默认为False
         """
         if set_level is None:
             set_level = self._exec_type()       # 设置set_level为None，自动获取当前运行模式
@@ -32,9 +35,13 @@ class Logger:
         self.__logger.setLevel(self.__level_dict[set_level] if set_level in self.__level_dict else set_level)
         if not os.path.exists(log_path):        # 创建日志目录
             os.makedirs(log_path)
-        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         handlers = list()
-        handlers.append(logging.FileHandler(os.path.join(log_path, log_name), encoding="utf-8"))
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        path = os.path.join(log_path, log_name)
+        if use_timed_rotating:
+            handlers.append(self._timed_rotating_file_handler(path, **timed_rotating_params))
+        else:
+            handlers.append(logging.FileHandler(path, encoding="utf-8"))
         if use_console:
             handlers.append(logging.StreamHandler())
         for handler in handlers:
@@ -43,6 +50,11 @@ class Logger:
 
     def _exec_type(self):
         return "DEBUG" if os.environ.get("IPYTHONENABLE") else "INFO"
+
+    def _timed_rotating_file_handler(self, path, wher="S", suffix="%Y-%m-%d(%H-%M-%S).log", backupCount=30):
+        file_handler = logging.handlers.TimedRotatingFileHandler(path, when=wher, interval=1, backupCount=backupCount)
+        file_handler.suffix = suffix
+        return file_handler
 
     def log(self, level, msg, *args, **kwargs):
         self.__logger.log(level, msg, *args, **kwargs)
@@ -67,3 +79,4 @@ class Logger:
 
     def debug(self, msg, *args, **kwargs):
         self.__logger.debug(msg, *args, **kwargs)
+
