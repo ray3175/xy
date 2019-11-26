4895d7f958ddcd749adcd0d5f4e3d60691b64452
ray3175 <57464715@qq.com>
2019-11-27 02:32:11 +0800

    新增文件编码格式读取

---
+++ xy/common/file/file_check.py
@@ -0,0 +1,15 @@
+import chardet
+
+
+class FileCheck:
+    @classmethod
+    def extract_encoding_from_bytes_str(cls, byte_str):
+        return chardet.detect(byte_str)["encoding"]
+
+    @classmethod
+    def extract_encoding_from_file(cls, file_path):
+        with open(file_path, "rb") as x:
+            byte_str = x.read()
+            x.close()
+        return cls.extract_encoding_from_bytes_str(byte_str)
+
