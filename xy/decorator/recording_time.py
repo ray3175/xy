32e17bfba24865cc46132103100abccfa54161bf
ray3175 <57464715@qq.com>
2021-03-26 00:04:50 +0800

    更新RecordingTime

--- xy/decorator/recording_time.py
+++ xy/decorator/recording_time.py
@@ -7,15 +7,15 @@ class RecordingTime:
     def __init__(self, name=""):
         self.__name = name
 
-    def print(self, start_call: Callable = lambda: "开始计时！", end_call: Callable = lambda: "结束计时，", info_call: Callable = lambda interval: "耗时：{}秒！".format(interval)):
+    def record(self, record_call: Callable = print, start_call: Callable = lambda *args, **kwargs: "开始计时！", end_call: Callable = lambda interval, *args, **kwargs: "结束计时，耗时：{}秒！".format(interval)):
         def interval(func):
             @wraps(func)
             def recording(*args, **kwargs):
-                print("{}{}".format(self.__name, start_call()))
+                record_call("{}{}".format(self.__name, start_call(*args, **kwargs)))
                 start = time.time()
                 _return = func(*args, **kwargs)
                 end = time.time()
-                print("{}{}{}".format(self.__name, end_call(), info_call(end - start)))
+                record_call("{}{}".format(self.__name, end_call(end - start, *args, **kwargs)))
                 return _return
             return recording
         return interval
